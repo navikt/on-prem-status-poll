59d00510df5c2efd4842ac87b1be58691d8b8afa
larsLotNav <lars.august.lotsberg@nav.no>
2023-04-13 11:10:49 +0200

    Added dtos, trying to get pollingservices

--- src/main/java/no/nav/onpremstatuspoll/OnPremStatusPollApplication.java
+++ src/main/java/no/nav/onpremstatuspoll/OnPremStatusPollApplication.java
@@ -3,13 +3,9 @@ package no.nav.onpremstatuspoll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.UUID;
+
+import java.util.List;
+
 
 @SpringBootApplication
 public class OnPremStatusPollApplication {
@@ -18,68 +14,65 @@ public class OnPremStatusPollApplication {
 
 		SpringApplication.run(OnPremStatusPollApplication.class, args);
 		try{
-			postStatus();
+			List<ServiceDto> services = PortalserverKlient.getPollingServices();
+			services.forEach(s ->
+
+							System.out.println(s.getName()+ " with polling url: "+ s.getPollingUrl())
+
+					);
+
+
 		}
 		catch (Exception e){
 			System.out.println(e);
 		}
-		try{
-			HttpURLConnection con = getPollingServices();
-			String body = readBody(con);
-			System.out.println(body);
-		}
-		catch (Exception e){
-			System.out.println(e);
-		}
-
-
 	}
-	private static void postStatus() throws IOException {
-		URL url = new URL ("https://statusholder.dev.intern.nav.no/status");
-		HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		con.setRequestMethod("POST");
-		con.setRequestProperty("Content-Type", "application/json");
-		con.setRequestProperty("Accept", "application/json");
-		con.setDoOutput(true);
-		UUID uuid = UUID.randomUUID();
-		String jsonInputString = "{\"serviceId\":\""+uuid.toString()+"\", \"status\": \"ISSUE\"}";
-		try(OutputStream os = con.getOutputStream()) {
-			byte[] input = jsonInputString.getBytes("utf-8");
-			os.write(input, 0, input.length);
-		}
-
-		try(BufferedReader br = new BufferedReader(
-				new InputStreamReader(con.getInputStream(), "utf-8"))) {
-			StringBuilder response = new StringBuilder();
-			String responseLine = null;
-			while ((responseLine = br.readLine()) != null) {
-				response.append(responseLine.trim());
-			}
-			System.out.println(response);
-			System.out.println("Postet status ok");
-		}
-
-	}
-
-	private static HttpURLConnection getPollingServices() throws IOException {
-		String urlString = "https://status-api.dev.nav.no/rest/Services";
-		URL url = new URL(urlString);
-		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-		con.setRequestMethod("GET");
-		return con;
-	}
-
-	private static String readBody(HttpURLConnection con) throws IOException {
-
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer content = new StringBuffer();
-		while ((inputLine = in.readLine()) != null) {
-			content.append(inputLine);
-		}
-		in.close();
-		return content.toString();
-	}
+//	private static void postStatus() throws IOException {
+//		URL url = new URL ("https://statusholder.dev.intern.nav.no/status");
+//		HttpURLConnection con = (HttpURLConnection)url.openConnection();
+//		con.setRequestMethod("POST");
+//		con.setRequestProperty("Content-Type", "application/json");
+//		con.setRequestProperty("Accept", "application/json");
+//		con.setDoOutput(true);
+//		UUID uuid = UUID.randomUUID();
+//		String jsonInputString = "{\"serviceId\":\""+uuid.toString()+"\", \"status\": \"ISSUE\"}";
+//		try(OutputStream os = con.getOutputStream()) {
+//			byte[] input = jsonInputString.getBytes("utf-8");
+//			os.write(input, 0, input.length);
+//		}
+//
+//		try(BufferedReader br = new BufferedReader(
+//				new InputStreamReader(con.getInputStream(), "utf-8"))) {
+//			StringBuilder response = new StringBuilder();
+//			String responseLine = null;
+//			while ((responseLine = br.readLine()) != null) {
+//				response.append(responseLine.trim());
+//			}
+//			System.out.println(response);
+//			System.out.println("Postet status ok");
+//		}
+//
+//	}
+//
+//	private static HttpURLConnection getPollingServices() throws IOException {
+//		String urlString = "https://status-api.dev.nav.no/rest/PollingServicesOnPrem";
+//		URL url = new URL(urlString);
+//		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//		con.setRequestMethod("GET");
+//		return con;
+//	}
+//
+//	private static String readBody(HttpURLConnection con) throws IOException {
+//
+//		BufferedReader in = new BufferedReader(
+//				new InputStreamReader(con.getInputStream()));
+//		String inputLine;
+//		StringBuffer content = new StringBuffer();
+//		while ((inputLine = in.readLine()) != null) {
+//			content.append(inputLine);
+//		}
+//		in.close();
+//		return content.toString();
+//	}
 
 }
---
+++ src/main/java/no/nav/onpremstatuspoll/RecordDto.java
@@ -0,0 +1,192 @@
+
+
+package no.nav.onpremstatuspoll;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+* RecordDto
+*/
+public class RecordDto {
+
+    private UUID serviceId = null;
+
+    private OffsetDateTime timestamp = null;
+
+    private StatusDto status = null;
+
+    private String description = null;
+
+    private String logLink = null;
+
+    private Integer responseTime = null;
+
+
+
+
+    public OffsetDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(OffsetDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public RecordDto timestamp(OffsetDateTime timestamp) {
+        this.timestamp = timestamp;
+        return this;
+    }
+
+    public UUID getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(UUID serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public RecordDto serviceId(UUID serviceId) {
+        this.serviceId = serviceId;
+        return this;
+    }
+
+    public StatusDto getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusDto status) {
+        this.status = status;
+    }
+
+    public RecordDto status(StatusDto status) {
+        this.status = status;
+        return this;
+    }
+
+    /**
+     * Get description
+     * @return description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public RecordDto description(String description) {
+        this.description = description;
+        return this;
+    }
+
+
+    public String getLogLink() {
+        return logLink;
+    }
+
+    public void setLogLink(String logLink) {
+        this.logLink = logLink;
+    }
+
+    public RecordDto logLink(String logLink) {
+        this.logLink = logLink;
+        return this;
+    }
+
+
+    public Integer getResponseTime() {
+        return responseTime;
+    }
+
+    public void setResponseTime(Integer responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    public RecordDto responseTime(Integer responseTime) {
+        this.responseTime = responseTime;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        RecordDto record = (RecordDto) o;
+        return  Objects.equals(this.getTimestamp(), record.getTimestamp()) &&
+                Objects.equals(this.getServiceId(), record.getServiceId()) &&
+                Objects.equals(this.getStatus(), record.getStatus()) &&
+                Objects.equals(this.getDescription(), record.getDescription()) &&
+                Objects.equals(this.getLogLink(), record.getLogLink()) &&
+                Objects.equals(this.getResponseTime(), record.getResponseTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTimestamp(), getServiceId(), getStatus(), getDescription(), getLogLink(), getResponseTime());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class RecordDto {\n");
+        sb.append("    timestamp: ").append(toIndentedString(getTimestamp())).append("\n");
+        sb.append("    serviceId: ").append(toIndentedString(getServiceId())).append("\n");
+        sb.append("    status: ").append(toIndentedString(getStatus())).append("\n");
+        sb.append("    description: ").append(toIndentedString(getDescription())).append("\n");
+        sb.append("    logLink: ").append(toIndentedString(getLogLink())).append("\n");
+        sb.append("    responseTime: ").append(toIndentedString(getResponseTime())).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+
+    public static class RecordDtoAdapter extends TypeAdapter<RecordDto> {
+        @Override
+        public void write(JsonWriter writer, RecordDto recordDto) throws IOException {
+            writer.beginObject();
+            writer.name("serviceId");
+            writer.value(recordDto.getServiceId().toString());
+            writer.name("timestamp");
+            writer.value(recordDto.getTimestamp().truncatedTo(ChronoUnit.SECONDS).toString());
+            writer.name("status");
+            writer.value(recordDto.getStatus().getValue());
+            writer.name("description");
+            writer.value(recordDto.getDescription());
+            writer.name("logLink");
+            writer.value(recordDto.getLogLink());
+            writer.name("responseTime");
+            writer.value(recordDto.getResponseTime());
+            writer.endObject();
+        }
+
+        @Override
+        public RecordDto read(JsonReader in) throws IOException {
+            return null;
+        }
+
+    }
+}
+
