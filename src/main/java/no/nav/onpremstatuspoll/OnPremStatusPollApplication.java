111f69f527aac97fc4889c1692605ffbbddd6813
larsLotNav <lars.august.lotsberg@nav.no>
2023-04-14 13:10:50 +0200

    Logging

--- src/main/java/no/nav/onpremstatuspoll/OnPremStatusPollApplication.java
+++ src/main/java/no/nav/onpremstatuspoll/OnPremStatusPollApplication.java
@@ -17,11 +17,12 @@ public class OnPremStatusPollApplication {
 			List<ServiceDto> services = PortalserverKlient.getPollingServices();
 			services.forEach(s ->{
 
-
+							System.out.println("-----------------------------");
 							System.out.println(s.getName()+" uuid: "+s.getId() +" with polling url: "+ s.getPollingUrl());
 							RecordDto recordDto = Poller.poll(s);
-							System.out.println(recordDto.getStatus());
-
+							System.out.println("status: "+ recordDto.getStatus());
+							System.out.println("Id: " + recordDto.getLogLink());
+							System.out.println("-----------------------------");
 					}
 
 
